c5a6827619668f2e7e13b91379e0ae3367286724
Kyle G Sessions <kage23@gmail.com>
2016-09-17 19:23:05 -0700

    My first checkers board! Wooooo

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var CheckersBoard = require('./CheckersBoard');
+
+function App () {
+	// this.props.etc
+	return (
+		<div>
+			<h1>WELCOME TO CHECKERSSSSSSSS</h1>
+
+			<CheckersBoard />
+		</div>
+	);
+}
+
+module.exports = App;
