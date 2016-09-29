42d36d184ab45d8f5d8ed0d43c18d12bbd2a02c9
Kyle Sessions <kage23@gmail.com>
2016-09-28 17:39:09 -0700

    Forgot to build

--- build/transformed.js
+++ build/transformed.js
@@ -21424,9 +21424,10 @@
 	var SiteHeader = __webpack_require__(173);
 	var GameHeader = __webpack_require__(174);
 	var CheckersBoard = __webpack_require__(175);
-	var TokenContainer = __webpack_require__(176);
-	var CheckersGameInfoContainer = __webpack_require__(178);
-	var CellContainer = __webpack_require__(180);
+	var Player = __webpack_require__(176);
+	var TokenContainer = __webpack_require__(177);
+	var CheckersGameInfoContainer = __webpack_require__(179);
+	var CellContainer = __webpack_require__(181);
 
 	var CheckersGame = React.createClass({
 	    displayName: 'CheckersGame',
@@ -21434,17 +21435,7 @@
 	    getInitialState: function () {
 	        return {
 	            cells: [],
-	            players: [{
-	                name: 'Player 1',
-	                color: 'red',
-	                tokens: [],
-	                positions: ['c0r7', 'c2r7', 'c4r7', 'c6r7', 'c1r6', 'c3r6', 'c5r6', 'c7r6', 'c0r5', 'c2r5', 'c4r5', 'c6r5']
-	            }, {
-	                name: 'Player 2',
-	                color: 'black',
-	                tokens: [],
-	                positions: ['c1r0', 'c3r0', 'c5r0', 'c7r0', 'c0r1', 'c2r1', 'c4r1', 'c6r1', 'c1r2', 'c3r2', 'c5r2', 'c7r2']
-	            }],
+	            players: [new Player('Player 1', 'red', this.config.player1StartingPositions, []), new Player('Player 2', 'black', this.config.player2StartingPositions, [])],
 	            currentTurn: 0,
 	            selectedToken: undefined
 	        };
@@ -21453,7 +21444,11 @@
 	    config: {
 	        boardSize: 8, // must be an even number. number of cells along each side of the board. board will be square.
 	        color: '#555', // color of playable squares
-	        secondaryColor: '#fff' // color of alternate squares
+	        secondaryColor: '#fff', // color of alternate squares
+	        player1StartingPositions: [// Array of cell IDs
+	        'c0r7', 'c2r7', 'c4r7', 'c6r7', 'c1r6', 'c3r6', 'c5r6', 'c7r6', 'c0r5', 'c2r5', 'c4r5', 'c6r5'],
+	        player2StartingPositions: [// Array of cell IDs
+	        'c1r0', 'c3r0', 'c5r0', 'c7r0', 'c0r1', 'c2r1', 'c4r1', 'c6r1', 'c1r2', 'c3r2', 'c5r2', 'c7r2']
 	    },
 
 	    generateBaseCellStyle: function () {
@@ -21475,7 +21470,7 @@
 
 	        currentPlayer = this.state.players[this.state.currentTurn % 2];
 
-	        currentPlayer.tokens.forEach(function (playerToken) {
+	        currentPlayer.getTokens().forEach(function (playerToken) {
 	            if (playerToken.key == tokenContainer.props.startingCell) tokenBelongsToPlayer = true;
 	        });
 
@@ -21490,14 +21485,57 @@
 	        }
 	    },
 
+	    getTokenState: function (tokenContainer) {
+	        debugger;
+	    },
+
 	    handleCellClick: function (cellContainer) {
-	        var newTurn;
+	        var newTurn,
+	            initialPosition,
+	            oppTokenArray,
+	            jumpedPiece = 'c',
+	            jumpedPiecePosition,
+	            opponent = this.state.players[(this.state.currentTurn + 1) % 2];
 
 	        if (cellContainer.props.highlighted) {
 	            newTurn = this.state.currentTurn + 1;
+	            initialPosition = this.state.selectedToken.state.cell;
 
 	            this.moveSelectedTokenToCell(cellContainer.props.id);
 	            this.resetHighlights();
+
+	            if (Math.abs(initialPosition[1] - cellContainer.props.id[1]) > 1) {
+	                debugger;
+	                // Piece got jumped. Calculate which cell got jumped
+	                if (parseInt(initialPosition[1]) < cellContainer.props.id[1]) {
+	                    jumpedPiece += parseInt(initialPosition[1]) + 1 + 'r';
+	                } else if (parseInt(initialPosition[1]) > cellContainer.props.id[1]) {
+	                    jumpedPiece += parseInt(initialPosition[1]) - 1 + 'r';
+	                }
+
+	                if (parseInt(initialPosition[3]) < cellContainer.props.id[3]) {
+	                    jumpedPiece += parseInt(initialPosition[3]) + 1;
+	                } else if (parseInt(initialPosition[3]) > cellContainer.props.id[3]) {
+	                    jumpedPiece += parseInt(initialPosition[3]) - 1;
+	                }
+
+	                // Find that cell in opponent's positions array
+	                oppTokenArray = opponent.getTokens();
+	                oppTokenArray.forEach(function (token) {
+	                    console.log(jumpedPiece);
+	                    debugger;
+	                    var blah = token.props.getState();
+	                }.bind(this));
+	                jumpedPiecePosition = oppTokenArray.indexOf(jumpedPiece);
+
+	                // Remove it
+	                oppTokenArray.splice(jumpedPiecePosition, 1);
+
+	                opponent.setPositions(oppTokenArray);
+
+	                // TODO: Remove jumped piece from opponent's tokens list
+	                // TODO: This might mean refactoring Players out of the game state into their own components maybe even though they won't render anything
+	            }
 
 	            this.setState({
 	                currentTurn: newTurn
@@ -21508,11 +21546,14 @@
 	    moveSelectedTokenToCell: function (cellId) {
 	        var currentPlayer = this.state.players[this.state.currentTurn % 2],
 	            initialPosition = this.state.selectedToken.state.cell,
-	            positionIndex = currentPlayer.positions.indexOf(initialPosition);
+	            playerPositions = currentPlayer.getPositions(),
+	            positionIndex = playerPositions.indexOf(initialPosition);
 
 	        this.state.selectedToken.setState({ cell: cellId });
 
-	        currentPlayer.positions[positionIndex] = cellId;
+	        playerPositions[positionIndex] = cellId;
+
+	        currentPlayer.setPositions(playerPositions);
 	    },
 
 	    highlightToken: function (token) {
@@ -21596,9 +21637,20 @@
 	    },
 
 	    generateToken: function (id, player, color, boardSize) {
-	        return React.createElement(TokenContainer, {
-	            color: color, type: 'circle', boardSize: boardSize, startingCell: id, key: id,
+	        var currentPlayer = this.state.players[player],
+	            newToken,
+	            newTokenRef;
+
+	        newToken = React.createElement(TokenContainer, {
+	            color: color, type: 'circle', boardSize: boardSize, startingCell: id, key: id, getState: this.getTokenState,
+	            ref: function (tokenContainer) {
+	                newTokenRef = tokenContainer;
+	            },
 	            owner: player, handleClick: this.handleTokenClick });
+
+	        // debugger;
+
+	        return newToken;
 	    },
 
 	    generateCellArray: function (currentArray, boardSize, inCellStyle, color, secondaryColor, highlightedArray) {
@@ -21631,7 +21683,7 @@
 	            }
 
 	            this.state.players.forEach(function (player, playerIndex) {
-	                if (player.positions.indexOf(id) > -1) {
+	                if (player.getPositions().indexOf(id) > -1) {
 	                    occupied = playerIndex;
 	                }
 	            });
@@ -21645,22 +21697,34 @@
 	    },
 
 	    componentDidMount: function () {
-	        var cellCount = this.config.boardSize * this.config.boardSize;
+	        // var cellCount = this.config.boardSize * this.config.boardSize;
 
-	        var players = [Object.assign({}, this.state.players[0]), Object.assign({}, this.state.players[1])];
+	        // var players = [
+	        //     Object.assign({}, this.state.players[0]),
+	        //     Object.assign({}, this.state.players[1])
+	        // ];
 
-	        var newPlayers = players.map(function (currentPlayer, index) {
-	            var newPlayer = Object.assign({}, currentPlayer);
-	            newPlayer.tokens = currentPlayer.positions.map(function (currentPosition) {
-	                return this.generateToken(currentPosition, index, currentPlayer.color, this.config.boardSize);
-	            }.bind(this));
-	            return newPlayer;
+	        // debugger;
+
+	        this.state.players.forEach(function (player, index) {
+	            // debugger;
+	            player.setTokens(player.getPositions().map(function (position) {
+	                return this.generateToken(position, index, player.getColor(), this.config.boardSize);
+	            }.bind(this)));
 	        }.bind(this));
+
+	        // var newPlayers = players.map(function (currentPlayer, index) {
+	        //     var newPlayer = Object.assign({}, currentPlayer);
+	        //     newPlayer.tokens = currentPlayer.positions.map(function (currentPosition) {
+	        //         return this.generateToken(currentPosition, index, currentPlayer.color, this.config.boardSize);
+	        //     }.bind(this));
+	        //     return newPlayer;
+	        // }.bind(this));
 
 	        var cells = this.generateCellArray([], this.config.boardSize, this.generateBaseCellStyle(), this.config.color, this.config.secondaryColor, []);
 
 	        this.setState({
-	            players: newPlayers,
+	            // players: newPlayers,
 	            cells: cells
 	        });
 	    },
@@ -21678,8 +21742,8 @@
 	                CheckersBoard,
 	                null,
 	                this.state.cells,
-	                this.state.players[0].tokens,
-	                this.state.players[1].tokens
+	                this.state.players[0].getTokens(),
+	                this.state.players[1].getTokens()
 	            )
 	        );
 	    }
@@ -21749,10 +21813,62 @@
 
 /***/ },
 /* 176 */
+/***/ function(module, exports) {
+
+	function Player(name, color, positions, tokens) {
+	    // debugger;
+	    var playerName, playerColor, playerPositions, playerTokens;
+
+	    playerName = name;
+	    playerColor = color;
+	    playerPositions = positions;
+	    playerTokens = tokens;
+
+	    // Getters
+
+	    this.getName = function () {
+	        return playerName;
+	    };
+
+	    this.getColor = function () {
+	        return playerColor;
+	    };
+
+	    this.getPositions = function () {
+	        return playerPositions;
+	    };
+
+	    this.getTokens = function () {
+	        return playerTokens;
+	    };
+
+	    // Setters
+
+	    this.setName = function (inName) {
+	        playerName = inName;
+	    };
+
+	    this.setColor = function (inColor) {
+	        playerColor = inColor;
+	    };
+
+	    this.setPositions = function (inPositions) {
+	        playerPositions = inPositions;
+	    };
+
+	    this.setTokens = function (inTokens) {
+	        playerTokens = inTokens;
+	    };
+	}
+
+	module.exports = Player;
+
+/***/ },
+/* 177 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var Token = __webpack_require__(177);
+	var Token = __webpack_require__(178);
 
 	var TokenContainer = React.createClass({
 	    displayName: 'TokenContainer',
@@ -21763,7 +21879,8 @@
 	        startingCell: React.PropTypes.string.isRequired,
 	        color: React.PropTypes.string,
 	        owner: React.PropTypes.number.isRequired,
-	        handleClick: React.PropTypes.func.isRequired
+	        handleClick: React.PropTypes.func.isRequired,
+	        getState: React.PropTypes.func.isRequired
 	    },
 
 	    getInitialState: function () {
@@ -21777,6 +21894,10 @@
 
 	    handleClick: function () {
 	        this.props.handleClick(this);
+	    },
+
+	    getState: function () {
+	        this.props.getState(this);
 	    },
 
 	    highlightToken: function () {
@@ -21827,7 +21948,7 @@
 	module.exports = TokenContainer;
 
 /***/ },
-/* 177 */
+/* 178 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
@@ -21845,11 +21966,11 @@
 	module.exports = Token;
 
 /***/ },
-/* 178 */
+/* 179 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var CheckersGameInfo = __webpack_require__(179);
+	var CheckersGameInfo = __webpack_require__(180);
 
 	var CheckersGameInfoContainer = React.createClass({
 	    displayName: 'CheckersGameInfoContainer',
@@ -21858,8 +21979,8 @@
 	        var currentPlayer = this.props.data.players[this.props.data.currentTurn % 2];
 
 	        return React.createElement(CheckersGameInfo, {
-	            name: currentPlayer.name,
-	            color: currentPlayer.color
+	            name: currentPlayer.getName(),
+	            color: currentPlayer.getColor()
 	        });
 	    }
 	});
@@ -21871,7 +21992,7 @@
 	module.exports = CheckersGameInfoContainer;
 
 /***/ },
-/* 179 */
+/* 180 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
@@ -21902,11 +22023,11 @@
 	module.exports = CheckersGameInfo;
 
 /***/ },
-/* 180 */
+/* 181 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var Cell = __webpack_require__(181);
+	var Cell = __webpack_require__(182);
 
 	var CellContainer = React.createClass({
 	    displayName: 'CellContainer',
@@ -21937,7 +22058,7 @@
 	module.exports = CellContainer;
 
 /***/ },
-/* 181 */
+/* 182 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
