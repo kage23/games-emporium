06c6bab4a71264306148b4f415d7c9228e3e7b9b
Kyle G Sessions <kage23@gmail.com>
2016-10-28 10:24:59 -0700

    issue #6 - List the variants on the main page

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -8,7 +8,20 @@ export default class GamesList extends React.Component {
                 <h2>Games List</h2>
 
                 <ul>
-                    <li><Link to="/games/checkers">Checkers</Link></li>
+                    <li>
+                        <p><Link to="/games/checkers">Checkers</Link> Includes:</p>
+                        <ul>
+                            <li>Regular Checkers</li>
+                            <li>Anti-Checkers</li>
+                            <li>Crowded Checkers</li>
+                            <li>Crowded Anti-Checkers</li>
+                            <li>Sparse Checkers</li>
+                            <li>Sparse Anti-Checkers</li>
+                            <li>Tiny Checkers</li>
+                            <li>Tiny Anti-Checkers</li>
+                            <li>Mule Checkers</li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         );
